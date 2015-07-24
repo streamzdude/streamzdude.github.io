da7c84269042d2bbb69c815c4ba33d9ede68586c
streamzdude <streamzderp@gmail.com>
2015-07-24 20:18:49 +0300

    adminn

--- admin.js
+++ admin.js
@@ -1,13 +1,37 @@
 var firebase = new Firebase("https://streamz.firebaseio.com/");
 
-$('#loginBut').click(login);
 
-function login() {
-	firebase.authWithOAuthPopup("google", function(error, authData) {
-	  if (error) {
-	    console.log("Login Failed!", error);
-	  } else {
-	    console.log("Authenticated successfully with payload:", authData);
-	  }
-	});
+function VM() {
+	var self = this;
+
+	this.authData = ko.observable(null);
+
+	this.login = function () {
+		firebase.authWithOAuthPopup("google", function(error, authData) {
+		  if (error) {
+		    console.log("Login Failed!", error);
+		  } else {
+		    console.log("Authenticated successfully with payload:", authData);
+		    self.authData(authData);
+		  }
+		});
+	};
+
+	this.logout = function() {
+		firebase.unauth();
+		self.authData(null);
+	}
+}
+
+var vm = new VM();
+ko.applyBindings(vm);
+
+
+var authdata = firebase.getAuth();
+if (authdata) {
+	console.log("Logged in: " + authdata.uid + " (" + authdata.provider + ")");
+	vm.authData(authdata);
+}
+else {
+	vm.login();
 }
