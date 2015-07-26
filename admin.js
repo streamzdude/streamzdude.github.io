fc2aab992c042d5869f94a0d7138ad11ba8f6247
streamzdude <streamzderp@gmail.com>
2015-07-26 13:23:41 +0300

    22

--- admin.js
+++ admin.js
@@ -1,9 +1,19 @@
 
+function Stream(stream) {
+	$.extend(this, stream);
+	this.visible = ko.observable(stream.visible !== false);
+	this.window = ko.observable();
+	this.name = ko.observable(stream.name);
+	this.type = ko.observable(stream.type);
+	this.src = ko.observable(stream.src);
+}
+
 function VM() {
 	var self = this;
 
 	this.initialized = ko.observable(false);
 	this.authData = ko.observable(null);
+	this.streams = ko.observableArray();
 
 	this.login = function () {
 		firebase.authWithOAuthPopup("google", function(error, authData) {
@@ -12,6 +22,7 @@ function VM() {
 		  } else {
 		    console.log("Authenticated successfully with payload:", authData);
 		    self.authData(authData);
+		    listenForStreams();
 		  }
 		});
 	};
@@ -25,10 +36,17 @@ function VM() {
 var vm = new VM();
 ko.applyBindings(vm);
 
-var firebase = new Firebase("https://streamz.firebaseio.com/");
-var authdata = firebase.getAuth();
-if (authdata) {
-	console.log("Logged in: " + authdata.uid + " (" + authdata.provider + ")");
-	vm.authData(authdata);
+var firebase = new Firebase("https://streamz.firebaseio.com/admin");
+var auth = firebase.getAuth();
+if (auth) {
+	vm.authData( auth );
+	listenForStreams();
 }
 vm.initialized(true);
+
+function listenForStreams() {
+	firebase.child("streams").on("value", function(snapshot) {
+		var fbStreams = snapshot.val();
+		console.log("fbStreams");
+	});
+}
