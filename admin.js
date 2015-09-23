f8e201fde04b8f2376d93146f3a82467c8d51956
streamzdude <streamzderp@gmail.com>
2015-09-23 04:57:10 +0300

    added streams to admin

--- admin.js
+++ admin.js
@@ -1,8 +1,8 @@
 
 function Stream(stream) {
+	stream = stream || {};
 	$.extend(this, stream);
 	this.visible = ko.observable(stream.visible !== false);
-	this.window = ko.observable();
 	this.name = ko.observable(stream.name);
 	this.type = ko.observable(stream.type);
 	this.src = ko.observable(stream.src);
@@ -14,6 +14,52 @@ function VM() {
 	this.initialized = ko.observable(false);
 	this.authData = ko.observable(null);
 	this.streams = ko.observableArray();
+
+	this.newStream = new Stream();
+	this.newStream.hasFocus = ko.observable(true);
+
+	this.removeStream = function(stream) {
+		self.streams.remove(stream);
+	};
+
+	this.save = function() {
+		if (!confirm('Sure you wanna overwrite all streams data?'))
+			return;
+
+		var streams = self.streams().reduce(function(currStreams, stream) {
+			currStreams[stream.name()] = {
+				type: stream.type(),
+				src: stream.src()
+			};
+			['bufferlength', 'image', 'width', 'height'].forEach(function(prop) {
+				if (typeof stream[prop] !== 'undefined') {
+					currStreams[stream.name()][prop] = stream[prop];
+				}
+			});
+			return currStreams;
+		}, {});
+
+		var streamsOrder = self.streams().map(function(stream) { return stream.name().replace(/,/g,'') }).join(",");
+
+		firebase.child("streamsOrder").set(streamsOrder);
+
+		firebase.child("streams").set(streams, function(error) {
+			if (error) {
+				console.log("failed saving streams:", error);
+			}
+		});
+	};
+
+	this.addStream = function() {
+		var stream = {
+			name: self.newStream.name(),
+			type: self.newStream.type(),
+			src: self.newStream.src(),
+			visible: true
+		};
+		self.streams.push(new Stream(stream));
+		self.newStream.name('').type('jwplayer').src('').hasFocus(true);
+	};
 
 	this.login = function () {
 		firebase.authWithOAuthPopup("google", function(error, authData) {
@@ -33,20 +79,40 @@ function VM() {
 	}
 }
 
+
+
+function listenForStreams() {
+	firebase.child("streams").on("value", function(snapshot) {
+		var fbStreams = snapshot.val();
+		console.log("received streams:", fbStreams);
+
+		var vmStreams = Object.keys(fbStreams).map(function(name) {
+			return new Stream($.extend({name:name}, fbStreams[name]));
+		});
+
+		vm.streams(vmStreams);
+
+	}, function (errorObject) {
+  		console.log("Reading streams failed: ", errorObject.code);
+	});
+}
+
+
+
+
 var vm = new VM();
 ko.applyBindings(vm);
 
 var firebase = new Firebase("https://streamz.firebaseio.com/admin");
 var auth = firebase.getAuth();
+//var auth = {uid:'lol', provider:'hehe'};
 if (auth) {
 	vm.authData( auth );
 	listenForStreams();
+
 }
 vm.initialized(true);
 
-function listenForStreams() {
-	firebase.child("streams").on("value", function(snapshot) {
-		var fbStreams = snapshot.val();
-		console.log("fbStreams");
-	});
-}
+
+
+
