db18e63b60dab6bdf2437211b8a57186f9a86c39
streamzdude <streamzderp@gmail.com>
2016-03-30 23:47:44 +0300

    remove shoutbox ip

--- shoutbox.js
+++ shoutbox.js
@@ -1,6 +1,5 @@
 var $ = require('jquery');
 var ko = require('knockout');
-var fetchIP = require('./fetch-ip');
 
 module.exports = createShoutbox;
 
@@ -11,7 +10,7 @@ else {
 	localStorage["streamzShoutboxName"] = shoutboxName = ('anon' + Math.floor(Math.random()*1000));
 }
 
-function createShoutbox(firebase, analyticsSession) {
+function createShoutbox(firebase, analytics) {
 	var shoutbox = {
 		items: ko.observableArray([]),
 		message: ko.observable(''),
@@ -22,17 +21,14 @@ function createShoutbox(firebase, analyticsSession) {
 			if (shoutbox.message().trim().length === 0 || 
 				shoutbox.name().trim().length === 0) return true;
 
-			fetchIP.then(function(ipData) {
-				firebase.child("shoutbox").push({
-					msg: shoutbox.message(),
-					name: shoutbox.name(),
-					time: Firebase.ServerValue.TIMESTAMP,
-					sessionId: analyticsSession.key(),
-					ip: ipData.ip
-				});
-
-				shoutbox.message('');
+			firebase.child("shoutbox").push({
+				msg: shoutbox.message(),
+				name: shoutbox.name(),
+				time: Firebase.ServerValue.TIMESTAMP,
+				sessionId: analytics.key()
 			});
+
+			shoutbox.message('');
 		}
 	};
 
