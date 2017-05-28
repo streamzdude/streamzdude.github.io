88b05ba53a708382036dbd20552301282f625fef
antishok <antishok@gmail.com>
2017-05-28 15:13:50 +0300

    https urls

--- src/fetch-ip.js
+++ src/fetch-ip.js
@@ -7,7 +7,12 @@ module.exports = dfd.promise();
 $.getJSON('http://ip-api.com/json').done(function(data) {
 	dfd.resolve({ipData:data, ip: data.query});
 }).fail(function() {
-	$.get('http://icanhazip.com').done(function(data) {
-		dfd.resolve({ip: data.trim()});
-	}).fail(dfd.reject);
+	$.get('https://freegeoip.net/json/').done(function(data) {
+		dfd.resolve({ip: data.ip, ipData: {countryCode: data.country_code, country: data.country_name}});
+	}).fail(function() {
+		$.get('https://icanhazip.com').done(function(data) {
+			dfd.resolve({ip: data.trim()});
+		}).fail(dfd.reject);
+	});
 });
+
