cdf63bc0f788bd32c0d03a723e1363fefa8cbd37
antishok <antishok@gmail.com>
2019-12-12 04:24:18 +0200

    cancel drag only on jwplayer control bar

--- src/windows.js
+++ src/windows.js
@@ -83,7 +83,7 @@ ko.bindingHandlers.window = {
 		});
 
 		elem.draggable({
-			cancel: '.player, object, .no-drag, input,textarea,button,select,option',
+			cancel: '.jw-controlbar, object, .no-drag, input,textarea,button,select,option',
 			start: function() {	vm.showOverlay(true) },
 			stop: function() {
 				vm.showOverlay(false);
